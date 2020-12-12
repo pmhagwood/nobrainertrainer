bf02833d25f873e44aff2a8646b10f9fc45369e4
Melanie Stuart <mmooradian90@yahoo.com>
2020-12-11 22:13:03 -0500

    seeding user table.

--- seeders/20201212024021-user-seeder.js
+++ seeders/20201212024021-user-seeder.js
@@ -8,7 +8,39 @@ module.exports = {
       email: 'mmooradian90@yahoo.com',
       createdAt: new Date(),
       updatedAt: new Date()
-    }]);
+    },{
+      userName: 'mariabyler',
+      password: 'password',
+      email: 'mariabyler98@gmail.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }{
+      userName: 'pmhagwood',
+      password: 'password',
+      email: '',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }{
+      userName: 'Rachelmoore2020',
+      password: 'password',
+      email: 'rachelmoore6822@gmail.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }{
+      userName: 'duncangw10',
+      password: 'password',
+      email: 'duncangw1@gmail.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }{
+      userName: 'DigitalAmnesia',
+      password: 'password',
+      email: 'mike@digitalamnesia.io',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    
+  ]);
   },
   down: async (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Users', null, {});
